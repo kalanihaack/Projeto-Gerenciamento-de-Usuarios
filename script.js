f38852e045c8497a001e641da9377522147328f5
Kalani <kalanihaack@gmail.com>
2026-05-07 23:05:06 -0300

    G6 - Usando if else para fazer as validacoes de genero

--- script.js
+++ script.js
@@ -14,6 +14,16 @@ var fields = document.querySelectorAll("#form-user-create [name]")
 
 fields.forEach(function(field, index){
 
-    console.log(field.id, field.name, field.value, field.checked, index)
+    if (field.name == "gender") {
+
+        if (field.checked) {
+            console.log("SIM", field)
+        }
+
+    } else{
+        console.log("NAO")
+    }
+
+    //console.log(field.id, field.name, field.value, field.checked, index)
 
 })
