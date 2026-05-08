cc1e88c63b5a5ca4df3fe817aa940a926f87891b
Kalani <kalanihaack@gmail.com>
2026-05-08 09:59:12 -0300

    G19 - Atualizando as estatisticas usando dataset

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -110,6 +110,8 @@ class UserController {
 
         let tr = document.createElement("tr")
 
+        tr.dataset.user = JSON.stringify(dataUser)
+
         tr.innerHTML =
         `
                     <td><img src="${dataUser.photo}" alt="User Image" class="img-circle img-sm"></td>
@@ -124,5 +126,27 @@ class UserController {
          `
 
          this.tableEl.appendChild(tr) //permite adicionar mais de um usuario na listagem, e retorna se o admin é sim ou nao
+
+         this.updateCount()
     }
-}
+
+    updateCount(){
+        
+        let numberUsers = 0
+        let numberAdmin = 0
+        
+         Array.from(this.tableEl.children).forEach(tr=>{
+
+            numberUsers++
+
+            let user = JSON.parse(tr.dataset.user)
+
+            if (user._admin) numberAdmin++ 
+
+
+         })
+
+        document.querySelector("#number-users").innerHTML = numberUsers
+        document.querySelector("#number-users-admin").innerHTML = numberAdmin
+    } 
+} //funcao que pega o array e atualiza as tabelas do html (contador de usuarios e administradores)
